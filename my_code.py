0fe4232d3171b9c19ceb62e229865cda55034c75
jibran <saleemjibran813@gmail.com>
2025-11-27 20:54:14 +0500

    first commit before dvc

---
+++ my_code.py
@@ -0,0 +1,15 @@
+import numpy as np 
+import pandas as pd
+import os
+
+dir = "Data"
+os.makedirs(dir ,exist_ok=True)
+
+path  = os.path.join(dir , "sample_data.csv")
+df = pd.DataFrame({
+    
+    'A': np.random.rand(5),
+    'B': np.random.rand(5),
+})
+df.to_csv(path , index = False)
+print(f"Data saved to {path}")
